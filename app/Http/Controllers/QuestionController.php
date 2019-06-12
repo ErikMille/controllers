5d1c19a3efdaaef709a9afbdce5ec0b7fc222dfb
ErikMille <miller.erik.1998@gmail>
2019-06-12 09:23:26 +0200

    show

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class QuestionController extends Controller
 {
     public function index(){
         return 'This is the list of questions';
+    }
+    public function show(){
+        return 'This is a detail of a question';
     }
 }
